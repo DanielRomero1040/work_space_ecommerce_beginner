b19861e145fb58501ec5866fa4bb6a4d31051290
Daniel Romero <danielromero1040@gmail.com>
2021-04-15 20:23:48 -0300

    Actualizacion

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ let acumulador = ``;
 let acumulador2 = ``;
 let acumulador3 = ``;
 let acumulador4 = ``;
+let acumuladorCarrito = 0;
+let acumuladorResumen = ``;
+let cantidad;
 let precios = [3000, 4500, 2000, 1500, 3230, 1800];
 let nombres = ['Lentes', 'Zapatos', 'Botas', 'Remeras', 'Buzos', 'Medias'];
 let descripcion = ['Protección UV', 'Zapatillas Puma Runner', 'Colección Invierno 2021', 'Todas las tallas', 'Gran variedad de diseños', 'Invisibles'];
@@ -13,19 +16,20 @@ for (let i = 0; i < precios.length; i++) {
 
   acumulador += `<div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
-    <a href="#"><img height="250" width="400" class="card-img-top" src="imagenes/${imagenes[i]}.jpg" alt=""></a>
-    <div class="card-body">
-    <h4 class="card-title">
-    <a href="#">${nombres[i]}</a>
-    </h4>
-    <h5> $${precios[i]}</h5>
-    <p class="card-text">${descripcion[i]}.</p>
+      <a href="#"><img height="250" width="400" class="card-img-top" src="imagenes/${imagenes[i]}.jpg" alt=""></a>
+      <div class="card-body">
+        <h4 class="card-title">
+        <a href="#">${nombres[i]}</a>
+        </h4>
+        <h5> $${precios[i]}</h5>
+        <p class="card-text">${descripcion[i]}.</p>
+      </div>
+      <div class="card-footer d-flex flex-row justify-content-center align-items-center">
+        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+        <button class="btn btn-primary ml-1" onClick="agregarAlCarrito(${precios[i]}, '${nombres[i]}')">Agreg. al Carrito</button>
+      </div>
     </div>
-    <div class="card-footer">
-    <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-    </div>
-    </div>
-    </div>`;
+  </div>`;
 }
 
 for (let i = 0; i < categorias.length; i++) {
@@ -57,10 +61,55 @@ for (let i = 0; i < banner.length; i++) {
   
 }
 
+let agregarAlCarrito = (precio, producto) => {
+  cantidad = parseInt(prompt("Ingrese cantidad del Producto que está comprando"));
+  indicadorCarrito(cantidad);
+  resumenDeCompra();
+  calculoSubTotal(cantidad, precio);  
+  alert(`va a comprar ${cantidad} ${producto} con precio ${precio}, el subtotal a pagar sería ${subTotal}`);
+}
 
+let indicadorCarrito = (cantidad) => {
+  acumuladorCarrito = acumuladorCarrito + cantidad;
+  document.getElementById("acumuladorCarrito").innerHTML = acumuladorCarrito;  
+  return acumuladorCarrito;
+}
+
+let calculoSubTotal = (cantidad, precio) => {
+  return subTotal = cantidad * precio;
+}
+
+let resumenDeCompra = () => {
+  acumuladorResumen += `<li class="list-group-item text-right mx-2">
+  cantidad x producto - precio (Pruebaaa)
+  <button class="btn btn-danger mx-5" style="margin-left:1rem">X</button>
+  </li>`;
+  document.getElementById("carrito").innerHTML = acumuladorResumen;  
+  return acumuladorResumen;  
+} 
 
 document.getElementById("productos").innerHTML = acumulador;
 document.getElementById("categorias").innerHTML = acumulador2;
 document.getElementById("indicadores").innerHTML = acumulador3;
 document.getElementById("carrusel").innerHTML = acumulador4;
+document.getElementById("acumuladorCarrito").innerHTML = acumuladorCarrito;
+document.getElementById("carrito").innerHTML = acumuladorResumen;
+
+
+// class Producto{
+//   constructor(nombre, precio, imagen, descripcion){
+//     this.nombre = nombre;
+//     this.precio = precio;
+//     this.imagen = imagen;
+//     this.descripcion = descripcion;    
+//   }
+
+//   iva () {
+//     console.log(precio*1.21)
+//   };
+// }
+
+// const Lentes = new Producto("Lentes", 1000, "/imagenes/lentes.jpg","Protección UV");
+// Lentes.iva();
+
 
